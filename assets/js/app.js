2945d2ae9c0453d76fec2d22d4e98efe47b892a5
PopoolaHakeem <hakeempopoola04@gmail.com>
2026-08-07 21:10:23 +0100

    Initial project setup

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// alert("iB Connect is loading...")
+
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const menuBtn = document.getElementById("menu-btn");
+
+    menuBtn.addEventListener("click", () => {
+
+        console.log("Mobile Menu");
+
+    });
+
+});
